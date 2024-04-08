0813e29b5e9d3db712b7cc5f0f3416cbff041ccf
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2024-04-08 11:26:31 -0700

    made runner.py clean

--- zero123plus/runner.py
+++ zero123plus/runner.py
@@ -1,11 +1,42 @@
 import torch
 import requests
 from PIL import Image
-from diffusers import Zero123PlusPipeline
-
-pipeline = Zero123PlusPipeline.from_pretrained(
-    "sudo-ai/zero123plus-v1.1", torch_dtype=torch.float16
+from diffusers.utils import load_image
+from diffusers import (
+    Zero123PlusPipeline,
+    StableVideoDiffusionPipeline,
+    DiffusionPipeline,
+    EulerAncestralDiscreteScheduler,
 )
+
+
+def load_image(path):
+    image = Image.open(path)
+    return image
+
+
+def load_pipeline_zero123(use_video=True):
+    pipe_zero123 = Zero123PlusPipeline.from_pretrained(
+        "sudo-ai/zero123plus-v1.1", torch_dtype=torch.float16
+    )
+
+    pipe_svd = StableVideoDiffusionPipeline.from_pretrained(
+        "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
+    )
+    pipe_svd.enable_model_cpu_offload()
+    if use_video:
+        pipe_zero123.unet = pipe_svd.unet
+    pipe_zero123.scheduler = EulerAncestralDiscreteScheduler.from_config(
+        pipe_zero123.scheduler.config, timestep_spacing='trailing'
+    )
+    pipe_zero123.to("cuda:0")
+
+    return pipe_zero123, pipe_svd
+
+
+pipe_zero123, pipe_svd = load_pipeline_zero123()
+cond = load_image("/localhome/aaa324/Generative Models/VideoPAPR/data/apple.png")
+result = pipe_zero123(cond, num_inference_steps=75, pipe_svd=pipe_svd, use_video=True).images[0]
 
 
 
